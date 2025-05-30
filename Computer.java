bd045e41d5cad6e62b15136660f9871555ea1dab
yannik404 <yannikschuldes@gmx.de>
2025-05-30 11:56:11 +0200

    viergewinntgame

---
+++ Computer.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Computer {
+
+    private final int maxColumns;
+
+    public Computer(int columns){
+        maxColumns = columns;
+    }
+
+    //Stein setzen
+    public int set(){
+        Random random = new Random();
+        return random.nextInt(0, maxColumns);
+    }
+}
